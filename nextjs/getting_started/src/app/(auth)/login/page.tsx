040b0f48969fc5d22fed570ca0f6d0c1d0bc53d2
ID-123 <unityplayer12@gmail.com>
2026-09-23 10:35:18 -0500

    feat: added login and register pages

---
+++ nextjs/getting_started/src/app/(auth)/login/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+
+export default function LoginForm() {
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log("Data received.");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-2">
+      <p>
+        <strong>Log In</strong>
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-center flex-col gap-1.5"
+      >
+        <input
+          className="border p-0.5"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="border p-0.5"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <button className="border p-0.5 hover:cursor-pointer" type="submit">
+          Send
+        </button>
+      </form>
+      <p>
+        Don't have an account?{" "}
+        <Link href={"/register"} className="underline hover:text-blue-500">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+}
---
+++ nextjs/getting_started/src/app/(auth)/register/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Link from "next/link";
+
+export default function RegisterForm() {
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const user = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log("Data received.");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-2">
+      <p>
+        <strong>Register</strong>
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center gap-1.5"
+      >
+        <input
+          className="border p-0.5"
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+
+        <input
+          className="border p-0.5"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+
+        <input
+          className="border p-0.5"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+
+        <button className="border p-0.5 hover:cursor-pointer" type="submit">
+          Send
+        </button>
+      </form>
+      <p>
+        Already hace an account?{" "}
+        <Link href={"/login"} className="underline hover:text-blue-600">
+          Log In
+        </Link>
+      </p>
+    </div>
+  );
+}
